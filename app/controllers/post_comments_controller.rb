677f5f930c068c73eb1b99719a03c389d288bc92
EC2 Default User <ec2-user@ip-172-31-8-88.ec2.internal>
2021-11-13 02:29:35 +0000

    [Update]meshiterroいいね機能追加

---
+++ app/controllers/post_comments_controller.rb
@@ -0,0 +1,23 @@
+class PostCommentsController < ApplicationController
+    
+    def create
+      post_image=PostImage.find(params[:post_image_id])
+      comment=current_user.post_comments.new(post_comment_params)
+    #   comment=PostComment.new(post_comment_params)
+    #   comment.user_id = current_user.id と同じ
+      comment.post_image_id=post_image.id
+      comment.save
+      redirect_to post_image_path(post_image)
+    end
+    
+    def destroy
+      PostComment.find_by(id:params[:id]).destroy
+    #   インスタンス変数を定義する必要がないのでまとめて表示
+      redirect_to post_image_path(params[:post_image_id])
+    end
+    
+    private
+    def post_comment_params
+        params.require(:post_comment).permit(:comment)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:"homes#top"
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  #親子関係 ネストする
+    resource :favorites, only: [:create, :destroy]
+    # showページ不要でidの受け渡し不要のためidをリクエストに含めないresource
+  end
 end
